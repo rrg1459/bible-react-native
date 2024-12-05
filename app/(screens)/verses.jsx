f54e8dcffa7d4f42588342eec842cb437e5d3f9d
rafaDev <rrg1459@hotmail.com>
2024-12-05 07:31:12 -0400

    feat: update screen verses with fillVerses adn versesAbbs

--- app/(screens)/verses.jsx
+++ app/(screens)/verses.jsx
@@ -1,36 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { useFocusEffect, useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { useFocusEffect } from 'expo-router';
 import { useRoute } from '@react-navigation/native';
 import { changeScreen } from '../redux/quoteSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import fillVerses from '../utils/fillVerses';
+import versesAbbs from '../utils/versesAbbs';
+import Verse from "../components/Verse.jsx";
 
 const VersesScreen = () => {
 
+  const languageValue = useSelector(state => state.quote.language);
+  const numVerses = useSelector(state => state.quote.numVerses);
+  const book = useSelector(state => state.quote.book);
+  const chapter = useSelector(state => state.quote.numChapter);
   const dispatch = useDispatch();
   const route = useRoute();
   const ScreenName = route.name;
+  const [verses, setVerses] = useState([]);
+  const [ShowVersesAbbs, setShowVersesAbbs] = useState('');
+
+  useEffect(() => {
+    setShowVersesAbbs(versesAbbs({ numVerses: numVerses, language: languageValue }));
+  }, [numVerses])
+
+  useEffect(() => {
+    setVerses(fillVerses({ book_id: book.id, chapter: chapter }));
+  }, [chapter])
+
   useFocusEffect(() => {
     dispatch(changeScreen(ScreenName));
   });
 
-  const params = useLocalSearchParams();
-  const { chapters, label } = params;
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>VersesScreen</Text>
-      <Text style={{fontSize: 40, color: 'skyblue'}}>Verses</Text>
-      <Text>XXX: {chapters}</Text>
+    <View style={styles.main} >
+      <Text style={styles.header}>
+        {book.label[languageValue]} {chapter}{ShowVersesAbbs}{/* imrpove this*/}
+      </Text>
+      <View style={styles.app}>
+        <FlatList
+          data={verses}
+          numColumns={1}
+          renderItem={({ item }) => <Verse verse={item} />}
+          keyExtractor={(verse) => String(verse.id)}
+        />
+      </View>
     </View>
   );
 }
 
 export default VersesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'skyblue',
+    flexDirection: 'column',
+    flex: 1
+  },
+  header: {
+    backgroundColor: '#7dfcd2',
+    fontSize: 25,
+    // padding: 5,
+    paddingTop: 30,
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+  app: {
+    cursor: "pointer",
+    // marginHorizontal: "auto",
+    // width: "98%"
+  },
+});
