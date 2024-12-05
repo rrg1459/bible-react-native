dfdffad45d1b987faa012b5df31ed6d99ed69bb2
rafaDev <rrg1459@hotmail.com>
2024-12-05 07:31:43 -0400

    feat: create component Verse

---
+++ app/components/Verse.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { updateVerses } from "../redux/quoteSlice";
+import cleanAndTidy from "../utils/cleanAndTidy";
+
+const Verse = (props) => {
+
+  const dispatch = useDispatch();
+  const { verse } = props;
+  const languageValue = useSelector(state => state.quote.language);
+  const numVerses = useSelector(state => state.quote.numVerses);
+  const [currentVerse, setCurrentVerse] = useState(false);
+  
+  const onPressVerse = () => {
+    dispatch(
+      updateVerses(
+        cleanAndTidy({verses: numVerses, verse: verse.verse, remove: currentVerse})
+      )
+    );
+    setCurrentVerse(!currentVerse);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={onPressVerse}>
+        <Text style={[styles.container, currentVerse ? styles.currentVerse : null]}>
+          {verse.verse}. {verse.text[languageValue]}
+        </Text>
+    </TouchableWithoutFeedback>
+  );
+}
+export default Verse;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    // textAlign: "center",
+    fontSize:20,
+    padding: 4,
+    margin: 2,
+    borderRadius: 5,
+    borderColor: "#fff"
+  },
+  currentVerse: {
+    backgroundColor: 'aquamarine',
+  },
+});
