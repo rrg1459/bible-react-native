7376158967352f9297aa61d6a1b423869c9d5f2a
rafaDev <rrg1459@hotmail.com>
2024-12-10 19:59:12 -0400

    feat: add books screen

---
+++ app/(screens)/books.jsx
@@ -0,0 +1,27 @@
+import { useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+import Settings from './settings';
+import ComponentBooks from '../components/Books';
+
+const renderScene = SceneMap({
+  screen: ComponentBooks,
+  settings: Settings,
+});
+
+export default function BooksScreen() {
+  const layout = useWindowDimensions();
+  const navigationState = {
+    index: 0, routes: [{ key: 'screen' }, { key: 'settings' }],
+  };
+
+  return (
+    <TabView
+      renderTabBar={() => null}
+      navigationState={navigationState}
+      renderScene={renderScene}
+      onIndexChange={() => navigationState}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+};
