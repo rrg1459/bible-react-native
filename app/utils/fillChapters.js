138097b020a0eb276ab58641597182bea5c477e8
rafaDev <rrg1459@hotmail.com>
2024-12-11 13:57:05 -0400

    feat: columns value

--- app/utils/fillChapters.js
+++ app/utils/fillChapters.js
@@ -1,14 +1,11 @@
-const fillChapters = ({ screen, chapters }) => {
+const fillChapters = ({ screen, chapters, columnsValue }) => {
 
   if (screen !== 'chapters') return [];
-
-  const numChapters = Number(chapters);
-  const filledChapters = [...Array(numChapters)].map((_, idx) => ({ id: idx + 1, show: true }));
-  const rest = numChapters % 5;
-  const remainingSlots = rest === 0 ? rest : (5 - rest);
+  const filledChapters = [...Array(chapters)].map((_, idx) => ({ id: idx + 1, show: true }));
+  const remainingSlots = columnsValue - (chapters % columnsValue);
   const dummyChapters = [...new Array(remainingSlots).keys()].map(
     (_, idx) => (
-      { id: idx + numChapters + 1, show: false }
+      { id: idx + chapters + 1, show: false }
     )
   )
 
