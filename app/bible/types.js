4ab13c7ca1734f173a6b5ab588b772a67d723aef
rafaDev <rrg1459@hotmail.com>
2024-12-16 08:56:34 -0400

    feat: return by type_id or all

--- app/bible/types.js
+++ app/bible/types.js
@@ -7,8 +7,12 @@ const types = (id) => {
     5: ["gospels", "evangelios"],
     6: ["facts", "hechos"],
     7: ["letters", "cartas"],
-    8: ["revelations", "revelaciones"]}
-  return types[id];
+    8: ["revelations", "revelaciones"],
+    9: ["old Testament", "viejo Testamento"],
+   10: ["new Testament", "nuevo Testamento"],
+   11: ["all testaments", "todos los testamentos"]
+  };
+  return id ? types[id] : types;
 };
 
 export default types;
