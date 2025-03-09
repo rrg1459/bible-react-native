db1a49aae9ec7bdf3bd457b8d34c59603924f26d
rafaDev <rrg1459@hotmail.com>
2025-03-09 09:23:31 -0400

    feat: updates the componente SettingsRetrievingFavorite

--- app/components/settings/SettingsRetrievingFavorite.jsx
+++ app/components/settings/SettingsRetrievingFavorite.jsx
@@ -2,35 +2,29 @@ import { useCallback } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux';
 import { updateFavorites } from '../../redux/quoteSlice';
-import { Storage } from '../../utils/storage';
-import { KEY } from '../../constants/storageKeys';
+import { saveItem } from "../../utils/setItems";
 
-const SettingsRetrievingFavorite = ({ language }) => {
-
+const SettingsRetrievingFavorite = ({ language, isTablet }) => {
   const dispatch = useDispatch();
   const retrieveFavorites = useSelector(state => state.quote.retrieveFavorites);
-
-  const saveFavorites = useCallback(async (favorites) => {
-    try {
-      await Storage.setItem(KEY.Favorites, favorites);
-    } catch (error) {
-      console.error('Failed to save font favorites verse:', error);
-    }
-  }, []);
-
   const onPress = useCallback(() => {
     dispatch(updateFavorites(retrieveFavorites));
-    saveFavorites(retrieveFavorites);
-  }, [dispatch, retrieveFavorites, saveFavorites]);
+    saveItem({ Favorites: retrieveFavorites });
+  }, [dispatch, retrieveFavorites]);
 
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={styles.button}
+      style={isTablet ? styles.buttonTablet : styles.button}
     >
-      <Text style={styles.textButton}>
-        {language ? 'Recuperar favoritos' : 'Retrieve favorites'}
-      </Text>
+      {isTablet
+        ? <Text style={styles.textButtonTablet}>
+          {language ? 'RECUPERAR\nFAVORITOS' : 'RETRIEVE\nFAVORITES'}
+          </Text>
+        : <Text style={styles.textButton}>
+          {language ? 'Recuperar favoritos' : 'Retrieve favorites'}
+          </Text>
+      }
     </TouchableOpacity>
   )
 }
@@ -56,5 +50,18 @@ const styles = StyleSheet.create({
     fontWeight: 'light',
     letterSpacing: 1.25,
     color: '#36395A',
+  },
+  buttonTablet: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d1ebf7',
+  },
+  textButtonTablet: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    letterSpacing: 1.25,
+    color: '#7e7f94',
+    textAlign: 'center',
   },
 });
