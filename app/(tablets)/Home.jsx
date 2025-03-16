e423d60fece118de000ccaaaa16f22cdd347cedc
rafaDev <rrg1459@hotmail.com>
2025-03-16 08:44:01 -0400

    feat: add hcustom hook to manage localStorage

--- app/(tablets)/Home.jsx
+++ app/(tablets)/Home.jsx
@@ -1,31 +1,116 @@
-import { useCallback, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
-import Header from '../components/Header';
-import Books from './Books';
-import Chapters from './Chapters';
-import Versicules from '../components/Versicules';
-import Footer from './Footer';
-import { useFocusEffect } from '@react-navigation/native';
-import { changeBookColumns } from '../redux/quoteSlice';
-import { useDispatch } from 'react-redux';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import Versicules from '../components/Versicules';
+import Header from '../components/Header';
+import Chapters from './Chapters';
+import Footer from './Footer';
+import Books from './Books';
+import {
+  updateBook,
+  updateDevice,
+  updateChapter,
+  updateLanguage,
+  updateFavorites,
+  changeBookColumns,
+  updateFontSizeVerse,
+  updateRetrieveFavorites,
+} from '../redux/quoteSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFocusEffect } from '@react-navigation/native';
+import { Storage } from '../utils/storage';
+import { KEY } from '../constants/storageKeys';
+
+const useFetchData = (dispatch, setLoadingStates) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [
+          lang,
+          sizeVerse,
+          favorites,
+          book,
+          chapter,
+        ] = await Promise.all([
+          Storage.getItem(KEY.Language),
+          Storage.getItem(KEY.FontSizeVerse),
+          Storage.getItem(KEY.Favorites),
+          Storage.getItem(KEY.Book),
+          Storage.getItem(KEY.Chapter),
+        ]);
+
+        if (lang || lang === 0) dispatch(updateLanguage(lang));
+        if (sizeVerse) dispatch(updateFontSizeVerse(sizeVerse));
+        if (favorites) {
+          dispatch(updateFavorites(favorites));
+          dispatch(updateRetrieveFavorites(favorites));
+        }
+        if (chapter) dispatch(updateChapter(chapter));
+        if (book) {
+          dispatch(updateBook(book));
+        }
+        else {
+          const tempBook = { id: 1, name: ["Genesis", "Génesis"], chapters: 50 };
+          dispatch(updateBook(tempBook));
+          dispatch(updateChapter(1));
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoadingStates(prevState => ({
+          ...prevState,
+          isLoadingLanguage: false,
+          isLoadingBooks: false,
+          isLoadingBook: false,
+          isLoadingChapters: false,
+          isLoadingVerses: false,
+        }));
+      }
+    };
+    fetchData();
+  }, [dispatch, setLoadingStates]);
+};
 
 const Home = () => {
   const dispatch = useDispatch();
 
-  const changeScreenOrientation = async () => {
-    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
-  };
+  const [loadingStates, setLoadingStates] = useState({
+    isLoadingLanguage: true,
+    isLoadingBooks: true,
+    isLoadingBook: true,
+    isLoadingChapters: true,
+    isLoadingVerses: true,
+  });
 
+  useFetchData(dispatch, setLoadingStates);
+  const languageValue = useSelector(state => state.quote.language);
+  async function changeScreenOrientation() {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+  }
   useEffect(() => {
     changeScreenOrientation();
-  }, []);
-
+  }, [])
   useFocusEffect(
     useCallback(() => {
       dispatch(changeBookColumns(11));
+      dispatch(updateDevice({ type: 'tablet' }));
     }, [dispatch])
   );
+  const isLoading = useMemo(() =>
+    loadingStates.isLoadingLanguage ||
+    loadingStates.isLoadingBooks ||
+    loadingStates.isLoadingBook ||
+    loadingStates.isLoadingChapters ||
+    loadingStates.isLoadingVerses,
+    [loadingStates]
+  );
+  if (isLoading) {
+    return (
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size='large' color='blue' />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.main}>
@@ -34,7 +119,7 @@ const Home = () => {
         <View style={styles.versicules}>
           <Versicules />
         </View>
-        <Footer />
+        <Footer language={languageValue} />
       </View>
       <View style={styles.right}>
         <View style={styles.books}>
@@ -50,6 +135,11 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+  activityIndicator: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   main: {
     backgroundColor: 'green',
     flex: 1,
