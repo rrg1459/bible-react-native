106d985f5981314d964794dc0e1ee8b7b632f5e7
rafaDev <rrg1459@hotmail.com>
2025-05-11 20:17:23 -0400

    feat: implement isNineInchTabletOrLarger utility to determine tablet size

---
+++ app/utils/isTablet.js
@@ -0,0 +1,46 @@
+import * as Device from 'expo-device';
+import { PixelRatio, Dimensions } from 'react-native';
+
+const isNineInchTabletOrLarger = () => {
+  const deviceTypeAsync = Device.getDeviceTypeAsync();
+  const { width, height } = Dimensions.get('window');
+  const pixelRatio = PixelRatio.get();
+
+  // Convertir puntos lógicos a píxeles físicos
+  const widthPx = width * pixelRatio;
+  const heightPx = height * pixelRatio;
+
+  // Calcular la diagonal en píxeles
+  const diagonalPx = Math.sqrt(widthPx * widthPx + heightPx * heightPx);
+
+  // Estimar la densidad de píxeles (DPI) - Para pantallas de alta densidad
+  // 300 inicial
+  // 200 ajustado
+  const estimatedDPI = 200;
+
+  // Calcular el tamaño de la pantalla en pulgadas
+  const screenDiagonalInches = diagonalPx / estimatedDPI;
+
+  // const isNineInchOrLarger = screenDiagonalInches >= 9 && deviceTypeAsync === Device.DeviceType.TABLET;
+  const isNineInchOrLarger = screenDiagonalInches >= 9 // && deviceTypeAsync === Device.DeviceType.TABLET;
+
+
+  const device = Device.deviceType; // 1 phone, 2 tablet, 3 web
+
+  console.log('-------------------START-------------------');
+  console.log('Tipo de Device:', Device);
+  console.log('Tipo de deviceTypeAsync:', deviceTypeAsync);
+  console.log('Tipo de device:', device);
+  console.log('Ancho (dp):', width);
+  console.log('Alto (dp):', height);
+  console.log('Relación de píxeles:', pixelRatio);
+  console.log('Diagonal (píxeles):', diagonalPx);
+  console.log('Diagonal (pulgadas):', screenDiagonalInches);
+  console.log('¿Es una tablet de 9 pulgadas o más?', isNineInchOrLarger);
+  console.log('Device.DeviceType.TABLET:', Device.DeviceType.TABLET);
+  console.log('-------------------END-------------------');
+  
+  return isNineInchOrLarger;
+};
+
+export default isNineInchTabletOrLarger;
