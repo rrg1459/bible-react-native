fdf806448eb2b8147864c90cc2dd921aedd38068
rafaDev <rrg1459@hotmail.com>
2025-05-11 20:27:48 -0400

    feat: enhance SettingsJesusQuotes component for tablet view and improve styling

--- app/components/settings/SettingsJesusQuotes.jsx
+++ app/components/settings/SettingsJesusQuotes.jsx
@@ -1,33 +1,39 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { useDispatch } from 'react-redux';
 import { updateShowJesusQuotes } from '../../redux/quoteSlice';
 import { saveItem } from "../../utils/setItems";
 
-const SettingsJesusQuotes = ({ language, showJesusQuotes }) => {
+const SettingsJesusQuotes = ({ language, showJesusQuotes, isTablet }) => {
   const dispatch = useDispatch();
   const onPress = useCallback(() => {
     dispatch(updateShowJesusQuotes(!showJesusQuotes));
     saveItem({ ShowJesusQuotes: !showJesusQuotes });
-
   }, [dispatch, showJesusQuotes]);
 
-  return (
+    // #D2042D cherry
+    const quoteStyle = useMemo(() => ({
+      color: showJesusQuotes ? '#D2042D' : '#7e7f94',
+    }), [showJesusQuotes]);
+
+    return (
     <TouchableOpacity
       onPress={onPress}
-      style={styles.button}
+      style={isTablet ? styles.buttonTablet : styles.button}
     >
-      {showJesusQuotes
-        ?
-        <Text style={styles.textButton}>
-          {language ? 'Ocultar frases de Jesús' : 'Hide Jesus quotes'}
-        </Text>
-        :
-        <Text style={styles.textButton}>
-          {language ? 'Destacar frases de Jesús' : 'Highlight Jesus quotes'}
-        </Text>
+      {
+        isTablet ?
+          <Text style={[quoteStyle, styles.textButtonTablet]}>{'JQ'}</Text>
+          :
+          showJesusQuotes ?
+            <Text style={styles.textButton}>
+              {language ? 'No destacar frases de Jesús' :  'Hide highlight Jesus quotes'}
+            </Text>
+            :
+            <Text style={styles.textButton}>
+              {language ? 'Destacar frases de Jesús' : 'Highlight Jesus quotes'}
+            </Text>
       }
-
     </TouchableOpacity>
   )
 };
@@ -54,5 +60,16 @@ const styles = StyleSheet.create({
     fontWeight: 'light',
     letterSpacing: 1.25,
     color: '#36395A',
+  },
+  buttonTablet: {
+    flex: 1,
+    backgroundColor: '#FFF0F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textButtonTablet: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    letterSpacing: 1.25,
   },
 });
