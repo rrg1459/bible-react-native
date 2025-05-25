370b038efd0ff698d82bcd8741da1ae3fe012c18
rafaDev <rrg1459@hotmail.com>
2025-05-25 17:17:16 -0400

    feat: update version number to E.145.1 in app.json and SettingsScreen component

--- app/(screens)/settings.jsx
+++ app/(screens)/settings.jsx
@@ -116,7 +116,7 @@ const SettingsScreen = () => {
         }
       </View>
       <View style={styles.footer}>
-        <Text style={styles.version}>{'E139.1'}</Text>
+        <Text style={styles.version}>{'E145.1'}</Text>
       </View>
     </>
   );
